e061395e94d332a8057d2e2d99dd27552f89ef9c
hilary888 <hilarykodji@gmail.com>
2022-05-09 12:01:28 +0000

    Remove helix repository and application

--- first-setup.sh
+++ first-setup.sh
@@ -11,6 +11,5 @@ sudo dnf groupupdate sound-and-video
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-sudo dnf copr enable varlad/helix
 
-sudo dnf install xclip google-chrome-stable vlc transmission gnome-tweaks gnome-extensions-app gnome-shell-extension-dash-to-dock gnome-shell-extension-netspeed gnome-shell-extension-gpaste micro git arc-theme paper-icon-theme papirus-icon-theme discord code helix
+sudo dnf install xclip google-chrome-stable vlc transmission gnome-tweaks gnome-extensions-app gnome-shell-extension-dash-to-dock gnome-shell-extension-netspeed gnome-shell-extension-gpaste micro git arc-theme paper-icon-theme papirus-icon-theme discord code
