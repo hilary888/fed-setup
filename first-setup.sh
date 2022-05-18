40dade087e1ba92930838783a121504f506c33e1
Hilary <hilarykodji@gmail.com>
2022-05-18 11:37:35 +0000

    add pomodoro and gnome shell user theme extension

--- first-setup.sh
+++ first-setup.sh
@@ -12,4 +12,4 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 
-sudo dnf install xclip google-chrome-stable vlc transmission gnome-tweaks gnome-extensions-app gnome-shell-extension-dash-to-dock gnome-shell-extension-netspeed gnome-shell-extension-gpaste micro git arc-theme paper-icon-theme papirus-icon-theme discord code
+sudo dnf install xclip google-chrome-stable vlc transmission gnome-tweaks gnome-extensions-app gnome-shell-extension-dash-to-dock gnome-shell-extension-netspeed gnome-shell-extension-gpaste micro git arc-theme paper-icon-theme papirus-icon-theme discord code gnome-pomodoro gnome-shell-extension-user-theme
